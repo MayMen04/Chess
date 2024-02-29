73c4cc648066c15fee4202e5ec560e40952db352
Maynor <maynormendieta84@gmail.com>
2024-02-29 15:35:56 -0800

    test 3

--- Rook.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Rook extends Piece{
-	private ArrayList<Point> point = new ArrayList<Point>();	
-	public Rook(String name, boolean color, int y, int x) {
-		super(name, color, y, x);
-	}	
-}
