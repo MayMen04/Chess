a697399a38138788ab82793a6b2636b7e2326f80
Maynor <maynormendieta84@gmail.com>
2024-02-29 16:00:56 -0800

    Revert "test 3"
    This reverts commit 73c4cc648066c15fee4202e5ec560e40952db352.

---
+++ Point.java
@@ -0,0 +1,15 @@
+
+public class Point {
+	private int x;
+	private int y;
+	public Point(int y, int x) {
+		this.y = y;
+		this.x = x;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+}
